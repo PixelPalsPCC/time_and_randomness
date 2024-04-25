88c436730aa6f4daf1040ed271f6dac099c06f05
George McKinney <george@hookstream.com>
2024-04-24 22:25:45 -0700

    unix time

--- sketch.js
+++ sketch.js
@@ -1,7 +1,38 @@
+let div
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 200)
+
+  background("#f00")
+
+  textSize(24)
+  fill(255, 255, 0)
+
+  text("Unix time since the epoch is ...", 40, 60)
+
+  // Create a div element and set its position.
+  div = createDiv('p5*js')
+  div.style('color', 'red')
+  div.style('font-size', '24px')
+  div.style('font-family', 'Arial')
+  div.style('background-color', 'yellow')
+  div.style('padding', '10px')
+  div.style('border-radius', '10px')
+  div.position(100, 80)
+
+  text("time = new Date().getTime()", 40, 175)
 }
 
 function draw() {
-  background(220);
+  let unixtime = new Date().getTime()
+  div.html(unixtime)
 }
+
+
+
+
+
+
+
+
+
